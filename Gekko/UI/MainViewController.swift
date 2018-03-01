3208f0ddfc5242094cd7ef2627aab1ced5a7abd3
Aleksandr Saliyenko <saliyenkoalexandr@gmail.com>
2018-03-02 01:00:37 +0200

    Delete print in refreshMainView

--- Gekko/UI/MainViewController.swift
+++ Gekko/UI/MainViewController.swift
@@ -478,8 +478,7 @@ class MainViewController : UIViewController,
         
         let handleOperationCompletion = {
             completedOperationsCount += 1
-            print(completedOperationsCount)
-
+        
             if (completedOperationsCount == requiredOperationsCount) {
                 sender.endRefreshing()
             }
