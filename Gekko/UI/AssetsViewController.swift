695b561a166fd893516adc55c3e1b9a51b1aefab
Serg Mykhailov <serg.mykhailov@gmail.com>
2018-07-05 13:29:03 +0300

    Copying keys to clipboard was implemented

--- Gekko/UI/AssetsViewController.swift
+++ Gekko/UI/AssetsViewController.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 protocol AssetsViewControllerDataSource : class {
 
@@ -67,10 +68,63 @@ class AssetsViewController : NavigatableViewController,
     // MARK: AssetCellDelegate implementation
 
     func assetCell(_ sender:AssetCell, didCaptureKey key:String) {
-        
+        UIPasteboard.general.string = key
+
+        presentNotification { }
     }
 
     // MARK: Internal methods
+
+    fileprivate func presentNotification(onCompletion:CompletionBlock) {
+        let label = UILabel()
+        label.text = NSLocalizedString("Key was copied to clipboard", comment:"Key copying message")
+        label.textAlignment = .center
+        label.lineBreakMode = .byWordWrapping
+
+        let labelContainerView = UIView()
+        labelContainerView.backgroundColor = UIColor(white:0, alpha:0.1)
+        labelContainerView.alpha = 0
+        labelContainerView.layer.cornerRadius = UIDefaults.CornerRadius
+
+        view.addSubview(labelContainerView)
+        labelContainerView.addSubview(label)
+
+        labelContainerView.setContentHuggingPriority(.required, for:.vertical)
+        labelContainerView.setContentHuggingPriority(.required, for:.horizontal)
+        labelContainerView.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.left.equalToSuperview().offset(UIDefaults.Spacing)
+            make.right.equalToSuperview().offset(-UIDefaults.Spacing)
+        }
+
+        label.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(UIDefaults.Spacing)
+            make.bottom.equalToSuperview().offset(-UIDefaults.Spacing)
+            make.left.equalToSuperview().offset(UIDefaults.Spacing)
+            make.right.equalToSuperview().offset(-UIDefaults.Spacing)
+        }
+
+        UIView.animateKeyframes(withDuration:AssetsViewController.NotificationAnimationDuration,
+                                delay:0,
+                                options:.calculationModeLinear,
+                                animations: {
+            UIView.addKeyframe(withRelativeStartTime:0,
+                               relativeDuration:1.0 / 8.0,
+                               animations: {
+                labelContainerView.alpha = 1
+                label.alpha = 1
+            })
+
+            UIView.addKeyframe(withRelativeStartTime: 1.0 / 4.0 * AssetsViewController.NotificationAnimationDuration,
+                               relativeDuration:1.0 / 4.0,
+                               animations: {
+                labelContainerView.alpha = 0
+                label.alpha = 0
+            })
+        }) { (_) in
+            labelContainerView.removeFromSuperview()
+        }
+    }
 
     // MARK: Outlets
 
@@ -81,4 +135,5 @@ class AssetsViewController : NavigatableViewController,
     fileprivate var supportedAssets:[Currency]?
 
     fileprivate static let CellIdentifier = "Asset Cell"
+    fileprivate static let NotificationAnimationDuration:TimeInterval = 2.0
 }
